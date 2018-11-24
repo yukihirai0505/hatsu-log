3022b1f4a5e44ff16a7bf55d3590843e6b827a66
Yuki Hirai <yukihirai0505@gmail.com>
2018-11-24 15:56:11 +0900

    Add leaflet

--- web/src/App.js
+++ web/src/App.js
@@ -14,6 +14,12 @@ import {
   Step,
   Table
 } from 'semantic-ui-react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet'
+
+Leaflet.Icon.Default.imagePath =
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/'
 
 const style = {
   h1: {
@@ -34,21 +40,41 @@ const style = {
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13,
+    }
   }
 
   async componentDidMount() {
-    const res = await fetch('https://script.google.com/macros/s/AKfycbyzNqxPtInjD8kDqZiD_kVr9MavY655S8oPR-uixuafnG-9YkU/exec')
+    const res = await fetch(
+      'https://script.google.com/macros/s/AKfycbyzNqxPtInjD8kDqZiD_kVr9MavY655S8oPR-uixuafnG-9YkU/exec'
+    )
     const json = await res.json()
     console.log(json)
   }
 
   render() {
+    const position = [this.state.lat, this.state.lng]
     return (
       <div>
-        <Header as='h1' content='Responsive UI Examples' style={style.h1} textAlign='center'/>
-        <Header as='h2' content='Basic Responsive' style={style.h2} textAlign='center'/>
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
 
-        <Header as='h3' textAlign='center' style={style.h3} content='Container'/>
+        <Header as="h1" content="Responsive UI Examples" style={style.h1} textAlign="center" />
+        <Header as="h2" content="Basic Responsive" style={style.h2} textAlign="center" />
+
+        <Header as="h3" textAlign="center" style={style.h3} content="Container" />
         <Container>
           <Segment.Group>
             <Segment>Content</Segment>
@@ -58,7 +84,7 @@ class App extends Component {
           </Segment.Group>
         </Container>
 
-        <Header as='h3' content='Text Container' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Text Container" style={style.h3} textAlign="center" />
         <Container text>
           <Segment.Group>
             <Segment>Content</Segment>
@@ -68,7 +94,7 @@ class App extends Component {
           </Segment.Group>
         </Container>
 
-        <Header as='h3' content='Stackable Grid' textAlign='center' style={style.h3}/>
+        <Header as="h3" content="Stackable Grid" textAlign="center" style={style.h3} />
         <Grid columns={2} stackable>
           <Grid.Column>
             <Segment>Content</Segment>
@@ -95,7 +121,7 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Doubling Grid' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Doubling Grid" style={style.h3} textAlign="center" />
         <Grid columns={3} doubling>
           <Grid.Column>
             <Segment>Content</Segment>
@@ -117,7 +143,7 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Doubling Stackable Grid' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Doubling Stackable Grid" style={style.h3} textAlign="center" />
         <Grid columns={3} doubling stackable>
           <Grid.Column>
             <Segment>Content</Segment>
@@ -139,7 +165,7 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Nested Stackable Grid' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Nested Stackable Grid" style={style.h3} textAlign="center" />
         <Grid columns={2}>
           <Grid.Column>
             <Grid columns={2} doubling stackable>
@@ -166,7 +192,7 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Stackable Grid Container' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Stackable Grid Container" style={style.h3} textAlign="center" />
         <Grid container columns={2} stackable>
           <Grid.Column>
             <Segment>Content</Segment>
@@ -188,7 +214,7 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Doubling Grid Container' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Doubling Grid Container" style={style.h3} textAlign="center" />
         <Grid container columns={3} doubling>
           <Grid.Column>
             <Segment>Content</Segment>
@@ -211,10 +237,10 @@ class App extends Component {
         </Grid>
 
         <Header
-          as='h3'
-          content='Doubling Stackable Grid Container'
+          as="h3"
+          content="Doubling Stackable Grid Container"
           style={style.h3}
-          textAlign='center'
+          textAlign="center"
         />
         <Grid container columns={3} doubling stackable>
           <Grid.Column>
@@ -237,9 +263,9 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h2' content='Device Adjustment' style={style.h2} textAlign='center'/>
+        <Header as="h2" content="Device Adjustment" style={style.h2} textAlign="center" />
 
-        <Header as='h3' content='Device Column Width' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Device Column Width" style={style.h3} textAlign="center" />
         <Grid>
           <Grid.Column computer={3} mobile={6} tablet={9}>
             <Segment>Content</Segment>
@@ -261,30 +287,30 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Device Visibility' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Device Visibility" style={style.h3} textAlign="center" />
         <Grid columns={4}>
-          <Grid.Column only='widescreen' widescreen={10}>
+          <Grid.Column only="widescreen" widescreen={10}>
             <Segment>Widescreen</Segment>
           </Grid.Column>
-          <Grid.Column only='widescreen' widescreen={6}>
+          <Grid.Column only="widescreen" widescreen={6}>
             <Segment>Widescreen</Segment>
           </Grid.Column>
-          <Grid.Column only='large screen' largeScreen={6}>
+          <Grid.Column only="large screen" largeScreen={6}>
             <Segment>Large Screen</Segment>
           </Grid.Column>
-          <Grid.Column only='large screen' largeScreen={10}>
+          <Grid.Column only="large screen" largeScreen={10}>
             <Segment>Large Screen</Segment>
           </Grid.Column>
-          <Grid.Column only='mobile tablet' mobile={8} tablet={8}>
+          <Grid.Column only="mobile tablet" mobile={8} tablet={8}>
             <Segment>Tablet and Mobile</Segment>
           </Grid.Column>
-          <Grid.Column only='mobile tablet' mobile={8} tablet={8}>
+          <Grid.Column only="mobile tablet" mobile={8} tablet={8}>
             <Segment>Tablet and Mobile</Segment>
           </Grid.Column>
-          <Grid.Column only='mobile' mobile={16}>
+          <Grid.Column only="mobile" mobile={16}>
             <Segment>Mobile</Segment>
           </Grid.Column>
-          <Grid.Row only='computer' columns={2}>
+          <Grid.Row only="computer" columns={2}>
             <Grid.Column>
               <Segment>Computer and Up</Segment>
             </Grid.Column>
@@ -292,23 +318,28 @@ class App extends Component {
               <Segment>Computer and Up</Segment>
             </Grid.Column>
           </Grid.Row>
-          <Grid.Column only='tablet'>
+          <Grid.Column only="tablet">
             <Segment>Tablet Only Content</Segment>
           </Grid.Column>
-          <Grid.Column only='tablet'>
+          <Grid.Column only="tablet">
             <Segment>Tablet Only Content</Segment>
           </Grid.Column>
-          <Grid.Column only='tablet'>
+          <Grid.Column only="tablet">
             <Segment>Tablet Only Content</Segment>
           </Grid.Column>
-          <Grid.Column only='tablet'>
+          <Grid.Column only="tablet">
             <Segment>Tablet Only Content</Segment>
           </Grid.Column>
         </Grid>
 
-        <Header as='h2' content='Responsive Grid with Variations' style={style.h2} textAlign='center'/>
+        <Header
+          as="h2"
+          content="Responsive Grid with Variations"
+          style={style.h2}
+          textAlign="center"
+        />
 
-        <Header as='h3' content='Stackable Divided Grid' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Stackable Divided Grid" style={style.h3} textAlign="center" />
         <Grid columns={2} container divided stackable>
           <Grid.Row>
             <Grid.Column>
@@ -337,12 +368,12 @@ class App extends Component {
         </Grid>
 
         <Header
-          as='h3'
-          content='Stackable Vertically Divided Grid'
+          as="h3"
+          content="Stackable Vertically Divided Grid"
           style={style.h3}
-          textAlign='center'
+          textAlign="center"
         />
-        <Grid columns={2} container divided='vertically' stackable>
+        <Grid columns={2} container divided="vertically" stackable>
           <Grid.Row>
             <Grid.Column>
               <Segment>Content</Segment>
@@ -369,7 +400,7 @@ class App extends Component {
           </Grid.Row>
         </Grid>
 
-        <Header as='h3' content='Celled Stackable Grid' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Celled Stackable Grid" style={style.h3} textAlign="center" />
         <Grid celled container stackable>
           <Grid.Row columns={2}>
             <Grid.Column>
@@ -401,10 +432,10 @@ class App extends Component {
         </Grid>
 
         <Header
-          as='h3'
-          content='Consecutive Doubling Stackable Grid'
+          as="h3"
+          content="Consecutive Doubling Stackable Grid"
           style={style.h3}
-          textAlign='center'
+          textAlign="center"
         />
         <Grid columns={3} container doubling stackable>
           <Grid.Column>
@@ -447,7 +478,7 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Grid Container' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Grid Container" style={style.h3} textAlign="center" />
         <Grid columns={3} container>
           <Grid.Column>
             <Segment>Content</Segment>
@@ -469,9 +500,9 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h1' content='Responsive Elements' style={style.h1} textAlign='center'/>
+        <Header as="h1" content="Responsive Elements" style={style.h1} textAlign="center" />
 
-        <Header as='h3' content='Responsive Vertical Divider' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Responsive Vertical Divider" style={style.h3} textAlign="center" />
         <Grid container columns={2} divided relaxed stackable>
           <Grid.Column>
             <Segment>Content</Segment>
@@ -481,7 +512,7 @@ class App extends Component {
           </Grid.Column>
         </Grid>
 
-        <Header as='h3' content='Responsive Table' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Responsive Table" style={style.h3} textAlign="center" />
         <Container>
           <Table celled>
             <Table.Header>
@@ -494,8 +525,8 @@ class App extends Component {
             <Table.Body>
               <Table.Row>
                 <Table.Cell>
-                  <Header as='h4' image>
-                    <Image rounded size='mini' src='/images/wireframe/square-image.png'/>
+                  <Header as="h4" image>
+                    <Image rounded size="mini" src="/images/wireframe/square-image.png" />
                     <Header.Content>
                       Lena
                       <Header.Subheader>Human Resources</Header.Subheader>
@@ -506,8 +537,8 @@ class App extends Component {
               </Table.Row>
               <Table.Row>
                 <Table.Cell>
-                  <Header as='h4' image>
-                    <Image rounded size='mini' src='/images/wireframe/square-image.png'/>
+                  <Header as="h4" image>
+                    <Image rounded size="mini" src="/images/wireframe/square-image.png" />
                     <Header.Content>
                       Matthew
                       <Header.Subheader>Fabric Design</Header.Subheader>
@@ -518,8 +549,8 @@ class App extends Component {
               </Table.Row>
               <Table.Row>
                 <Table.Cell>
-                  <Header as='h4' image>
-                    <Image rounded size='mini' src='/images/wireframe/square-image.png'/>
+                  <Header as="h4" image>
+                    <Image rounded size="mini" src="/images/wireframe/square-image.png" />
                     <Header.Content>
                       Lindsay
                       <Header.Subheader>Entertainment</Header.Subheader>
@@ -530,8 +561,8 @@ class App extends Component {
               </Table.Row>
               <Table.Row>
                 <Table.Cell>
-                  <Header as='h4' image>
-                    <Image rounded size='mini' src='/images/wireframe/square-image.png'/>
+                  <Header as="h4" image>
+                    <Image rounded size="mini" src="/images/wireframe/square-image.png" />
                     <Header.Content>
                       Mark
                       <Header.Subheader>Executive</Header.Subheader>
@@ -544,11 +575,11 @@ class App extends Component {
           </Table>
         </Container>
 
-        <Header as='h3' content='Responsive Menu' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Responsive Menu" style={style.h3} textAlign="center" />
         <Container>
           <Menu stackable>
             <Menu.Item>
-              <img src='/logo.png'/>
+              <img src="/logo.png" />
             </Menu.Item>
             <Menu.Item>Features</Menu.Item>
             <Menu.Item>Testimonials</Menu.Item>
@@ -556,13 +587,13 @@ class App extends Component {
           </Menu>
         </Container>
 
-        <Header as='h3' content='Responsive Item' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Responsive Item" style={style.h3} textAlign="center" />
         <Container>
           <Item.Group divided>
             <Item>
-              <Item.Image src='/images/wireframe/image.png'/>
+              <Item.Image src="/images/wireframe/image.png" />
               <Item.Content>
-                <Item.Header as='a'>Content Header</Item.Header>
+                <Item.Header as="a">Content Header</Item.Header>
                 <Item.Meta>
                   <span>Date</span>
                   <span>Category</span>
@@ -571,16 +602,16 @@ class App extends Component {
                   A description which may flow for several lines and give context to the content.
                 </Item.Description>
                 <Item.Extra>
-                  <Image avatar circular src='/images/wireframe/square-image.png'/>
+                  <Image avatar circular src="/images/wireframe/square-image.png" />
                   Username
                 </Item.Extra>
               </Item.Content>
             </Item>
 
             <Item>
-              <Item.Image src='/images/wireframe/image.png'/>
+              <Item.Image src="/images/wireframe/image.png" />
               <Item.Content>
-                <Item.Header as='a'>Content Header</Item.Header>
+                <Item.Header as="a">Content Header</Item.Header>
                 <Item.Meta>
                   <span>Date</span>
                   <span>Category</span>
@@ -589,18 +620,18 @@ class App extends Component {
                   A description which may flow for several lines and give context to the content.
                 </Item.Description>
                 <Item.Extra>
-                  <Button floated='right' primary>
+                  <Button floated="right" primary>
                     Primary
-                    <Icon name='chevron right'/>
+                    <Icon name="chevron right" />
                   </Button>
                   <Label>Limited</Label>
                 </Item.Extra>
               </Item.Content>
             </Item>
             <Item>
-              <Item.Image src='/images/wireframe/image.png'/>
+              <Item.Image src="/images/wireframe/image.png" />
               <Item.Content>
-                <Item.Header as='a'>Content Header</Item.Header>
+                <Item.Header as="a">Content Header</Item.Header>
                 <Item.Meta>
                   <span>Date</span>
                   <span>Category</span>
@@ -609,9 +640,9 @@ class App extends Component {
                   A description which may flow for several lines and give context to the content.
                 </Item.Description>
                 <Item.Extra>
-                  <Button primary floated='right'>
+                  <Button primary floated="right">
                     Primary
-                    <Icon name='chevron right'/>
+                    <Icon name="chevron right" />
                   </Button>
                 </Item.Extra>
               </Item.Content>
@@ -619,17 +650,17 @@ class App extends Component {
           </Item.Group>
         </Container>
 
-        <Header as='h3' content='Responsive Steps' style={style.h3} textAlign='center'/>
+        <Header as="h3" content="Responsive Steps" style={style.h3} textAlign="center" />
 
         <Container style={style.last}>
           <Step.Group fluid>
-            <Step icon='plane' title='Shipping' description='Choose your shipping options'/>
-            <Step active icon='dollar' title='Billing' description='Enter billing information'/>
+            <Step icon="plane" title="Shipping" description="Choose your shipping options" />
+            <Step active icon="dollar" title="Billing" description="Enter billing information" />
             <Step
               disabled
-              icon='info circle'
-              title='Confirm Order'
-              description='Verify order details'
+              icon="info circle"
+              title="Confirm Order"
+              description="Verify order details"
             />
           </Step.Group>
         </Container>
